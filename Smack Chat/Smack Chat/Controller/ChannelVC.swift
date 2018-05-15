70eb53f9b37704b1a0c2614fda6d6c7dd8f0c15f
alishgiri <es.alish.giri@gmail.com>
2018-05-15 09:48:46 +0545

    channel display optimised, chat title channel selected

--- Smack Chat/Smack Chat/Controller/ChannelVC.swift
+++ Smack Chat/Smack Chat/Controller/ChannelVC.swift
@@ -23,7 +23,8 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChanged(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
-
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.channelsLoaded(_:)), name: NOTIF_CHANNELS_LOADED, object: nil)
+        
         SocketService.instance.getChannnel { (success) in
             if success {
                 self.tableView.reloadData()
@@ -38,10 +39,17 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @objc func userDataDidChanged(_ notify: Notification) {
         setupUserInfo()
     }
+    @objc func channelsLoaded(_ notify: Notification) {
+        tableView.reloadData()
+    }
+    
+    
     @IBAction func btnAddChannel(_ sender: Any) {
-        let addChannel = AddChannelVC()
-        addChannel.modalPresentationStyle = .custom
-        present(addChannel, animated: true, completion: nil)
+        if AuthService.instance.isLoggedIn {
+            let addChannel = AddChannelVC()
+            addChannel.modalPresentationStyle = .custom
+            present(addChannel, animated: true,  completion: nil)
+        }
     }
     
     @IBAction func btnLogin(_ sender: UIButton) {
@@ -64,7 +72,7 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             btnLoginOutlet.setTitle("Login", for: [])
             imgUserImage.image = UIImage(named: "menuProfileIcon")
             imgUserImage.backgroundColor = UIColor.clear
-            imgUserImage.clipsToBounds = true
+            tableView.reloadData()
         }
     }
     
@@ -84,5 +92,13 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return MessageService.instance.channels.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let channel = MessageService.instance.channels[indexPath.row]
+        MessageService.instance.selectedChannel = channel
+        NotificationCenter.default.post(name: NOTIF_CHANNEL_SELECTED, object: nil)
+        
+        self.revealViewController().revealToggle(animated: true)
     }
 }
--- Smack Chat/Smack Chat/Controller/ChatVC.swift
+++ Smack Chat/Smack Chat/Controller/ChatVC.swift
@@ -11,12 +11,16 @@ import UIKit
 class ChatVC: UIViewController {
     
     @IBOutlet weak var btnMenu: UIButton!
+    @IBOutlet weak var lblChannelName: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         btnMenu.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer()) // SWIPE GESTURE
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChanged(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIF_CHANNEL_SELECTED, object: nil)
         
         if AuthService.instance.isLoggedIn {
             AuthService.instance.findUserByEmail { (success) in
@@ -26,6 +30,33 @@ class ChatVC: UIViewController {
         
         MessageService.instance.findAllChannel { (success) in
             
+        }
+    }
+    
+    @objc func userDataDidChanged(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            // get channels
+            onLoginGetMessages()
+        } else {
+            lblChannelName.text = "Please Log In"
+        }
+    }
+    
+    @objc func channelSelected(_ notif: Notification) {
+        updateWithChannel()
+    }
+    
+    func updateWithChannel() {
+        let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
+        lblChannelName.text = "#\(channelName)"
+    }
+    
+    func onLoginGetMessages() {
+        MessageService.instance.findAllChannel { (success) in
+            if success {
+                // Do stuff with channels
+                
+            }
         }
     }
 
